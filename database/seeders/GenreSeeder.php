19ec7e6e86e49729a843bcfb618be761cdfc7a48
Y0shito <75707750+Y0shito@users.noreply.github.com>
2023-04-14 19:31:37 +0900

    genresに挿入の際、ID:1から入るよう修正

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -14,11 +14,11 @@ class GenreSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement("INSERT INTO genres (name, created_at, updated_at)
-        VALUES ('アクション', NOW(), NOW()),
-               ('ホラー', NOW(), NOW()),
-               ('SF', NOW(), NOW()),
-               ('ドキュメンタリー', NOW(), NOW()),
-               ('ロマンス', NOW(), NOW());");
+        DB::statement("INSERT INTO genres (id, name, created_at, updated_at)
+        VALUES (1,'アクション', NOW(), NOW()),
+               (2,'ホラー', NOW(), NOW()),
+               (3,'SF', NOW(), NOW()),
+               (4,'ドキュメンタリー', NOW(), NOW()),
+               (5,'ロマンス', NOW(), NOW());");
     }
 }
