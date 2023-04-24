035b9cac46520e79323c06f03f68429bcfb8efb8
Y0shito <75707750+Y0shito@users.noreply.github.com>
2023-04-24 19:28:42 +0900

    微修正

--- resources/views/admin/schedule/create.blade.php
+++ resources/views/admin/schedule/create.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>スケジュール作成</title>
+    <title>{{ $movie->title }}/スケジュール作成</title>
 </head>
 
 <body>
-    @include('components.admin.header', ['title' => 'スケジュール一覧'])
+    @include('components.admin.header', ['title' => "{$movie->title}/スケジュール作成"])
     @include('components.validationErrors')
     @include('components.flashmessage')
 
